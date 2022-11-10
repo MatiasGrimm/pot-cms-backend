756ce2bc8a310467ae2c387ca56874742e8572fa
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2022-11-10 17:00:28 +0100

    Viewmodel for Locations
    Create SimpleLocationViewModel.cs
    Create LocationViewModel.cs

---
+++ API/ViewModels/LocationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PotShop.API.ViewModels
+{
+    public class LocationViewModel : SimpleLocationViewModel
+    {
+        public int Type { get; set; }
+
+        public bool IsDisabled { get; set; }
+
+        public Guid InventoryId { get; set; }
+    }
+}
---
+++ API/ViewModels/SimpleLocationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PotShop.API.ViewModels
+{
+    public class SimpleLocationViewModel
+    {
+        public Guid ManagerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
