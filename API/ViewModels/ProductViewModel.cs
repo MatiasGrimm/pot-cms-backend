a59259c3eedf3d4978976e8e2d4b30979611acc2
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2022-11-11 18:01:15 +0100

    Add id to product viewmodel

--- API/ViewModels/ProductViewModel.cs
+++ API/ViewModels/ProductViewModel.cs
@@ -4,6 +4,8 @@ namespace PotShop.API.ViewModels
 {
     public class ProductViewModel
     {
+        public Guid? Id { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
