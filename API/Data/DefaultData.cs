e212f339e0383a752d9310305d186b74ed021781
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2022-11-15 09:06:52 +0100

    Update DefaultData.cs

--- API/Data/DefaultData.cs
+++ API/Data/DefaultData.cs
@@ -7,11 +7,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PotShop.API.Models.Entities.Enums;
 
 namespace PotShop.API.Data
 {
     public static class DefaultData
     {
+        private static async Task AddStaff(UserManager<ApiUser> userMgr, string username, string name, Location location)
+        {
+            var user = new ApiUser()
+            {
+                UserName = username,
+                Email = username,
+                Name = name,
+                Location = location,
+            };
+
+            var staffAccess = new StaffAccess()
+            {
+                Location = location,
+                Staff = user,
+                Access = Access.GetInventory
+            };
+
+            user.StaffAccess = staffAccess;
+
+            await userMgr.CreateAsync(user, "P@ssw0rd");
+        }
+        private static async Task AddStaffWithAllPerms(UserManager<ApiUser> userMgr, string username, string name, Location location)
+        {
+            var user = new ApiUser()
+            {
+                UserName = username,
+                Email = username,
+                Name = name,
+                Location = location,
+            };
+
+            var staffAccess = new StaffAccess()
+            {
+                Location = location,
+                Staff = user,
+                Access = Access.All
+            };
+
+            user.StaffAccess = staffAccess;
+
+            await userMgr.CreateAsync(user, "P@ssw0rd");
+
+            await userMgr.AddToRolesAsync(user, new string[] { AuthRoles.Admin, AuthRoles.Manager });
+        }
+
         public static async Task PopulateAccounts(IServiceProvider serviceProvider)
         {
             var context = serviceProvider.GetRequiredService<ApiDbContext>();
@@ -30,48 +76,53 @@ namespace PotShop.API.Data
                 };
             }
 
+
+            if (!await roleManager.RoleExistsAsync(AuthRoles.Admin))
+            {
+                await roleManager.CreateAsync(new IdentityRole() { Name = AuthRoles.Admin });
+            }
+
+            if (!await roleManager.RoleExistsAsync(AuthRoles.Manager))
+            {
+                await roleManager.CreateAsync(new IdentityRole() { Name = AuthRoles.Manager });
+            }
+
             if (!context.Users.Any())
             {
-                var mgc = new ApiUser()
-                {
-                    UserName = "mgc@kodeministeriet.dk",
-                    Email = "mgc@kodeministeriet.dk",
-                    Name = "Matias Grimm",
-                    Location = defaultLocation
-                };
+                await AddStaffWithAllPerms(userMgr, "mgc@kodeministeriet.dk", "Matias Grimm", defaultLocation);
 
-                var testManager = new ApiUser()
-                {
-                    UserName = "testManager@testing.dk",
-                    Email = "testManager@testing.dk",
-                    Name = "Test Manager",
-                    Location = defaultLocation
-                };
+                //var mgc = new ApiUser()
+                //{
+                //    UserName = "mgc@kodeministeriet.dk",
+                //    Email = "mgc@kodeministeriet.dk",
+                //    Name = "Matias Grimm",
+                //    Location = defaultLocation
+                //};
 
-                var testEmployee = new ApiUser()
-                {
-                    UserName = "TestEmployee@testing.dk",
-                    Email = "TestEmployee@testing.dk",
-                    Name = "Test Employee",
-                    Location = defaultLocation
-                };
 
-                await userMgr.CreateAsync(mgc, "p@ssw0rd");
-                await userMgr.CreateAsync(testEmployee, "p@ssw0rd");
-                await userMgr.CreateAsync(testManager, "p@ssw0rd");
+                //var testManager = new ApiUser()
+                //{
+                //    UserName = "testManager@testing.dk",
+                //    Email = "testManager@testing.dk",
+                //    Name = "Test Manager",
+                //    Location = defaultLocation
+                //};
 
-                if (!await roleManager.RoleExistsAsync(AuthRoles.Admin))
-                {
-                    await roleManager.CreateAsync(new IdentityRole() { Name = AuthRoles.Admin });
-                }
+                //var testEmployee = new ApiUser()
+                //{
+                //    UserName = "TestEmployee@testing.dk",
+                //    Email = "TestEmployee@testing.dk",
+                //    Name = "Test Employee",
+                //    Location = defaultLocation
+                //};
 
-                if (!await roleManager.RoleExistsAsync(AuthRoles.Manager))
-                {
-                    await roleManager.CreateAsync(new IdentityRole() { Name = AuthRoles.Manager });
-                }
+                //await userMgr.CreateAsync(mgc, "p@ssw0rd");
+                //await userMgr.CreateAsync(testEmployee, "p@ssw0rd");
+                //await userMgr.CreateAsync(testManager, "p@ssw0rd");
 
-                await userMgr.AddToRolesAsync(mgc, new string[] { AuthRoles.Admin, AuthRoles.Manager } );
-                await userMgr.AddToRolesAsync(testManager, new string[] {AuthRoles.Manager});
+
+                //await userMgr.AddToRolesAsync(mgc, new string[] { AuthRoles.Admin, AuthRoles.Manager } );
+                //await userMgr.AddToRolesAsync(testManager, new string[] {AuthRoles.Manager});
             }
             if (!context.Products.Any())
             {
